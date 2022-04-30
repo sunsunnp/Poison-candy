239a49b1c5f214df1a186f1b579b2badce0e95f3
issaraporn <issaraporn.s0507@gmail.com>
2022-05-01 00:07:45 +0700

    update random page and style

--- public/scripts/js/index.js
+++ public/scripts/js/index.js
@@ -1,3 +1,8 @@
-function test() {
-    console.log('test');
-}
+function gotoDiv(id) {
+    const yOffset = -75;
+    const element = document.getElementById(id);
+    const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  
+    window.scrollTo({ top: y, behavior: 'smooth' });
+  }
+  
