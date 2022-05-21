61af59b5e5b825e5a76f87abe7f03821024afee9
nakorata_sunsun <nakorata.p@i3gateway.com>
2022-05-21 16:00:19 +0700

    edit result 1

--- scripts/js/index.js
+++ scripts/js/index.js
@@ -1,7 +1,8 @@
 function gotoDiv(id) {
-  const yOffset = -75;
-  const element = document.getElementById(id);
-  const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-
-  window.scrollTo({ top: y, behavior: 'smooth' });
-}
+    const yOffset = -75;
+    const element = document.getElementById(id);
+    const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  
+    window.scrollTo({ top: y, behavior: 'smooth' });
+  }
+  
